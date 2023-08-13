c7801940c973c50ae6f318eedc3ac1ed4febfde8
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-13 15:44:26 +0100

    Finished navbar and userMenu layout

--- components/navbar/UserMenu.tsx
+++ components/navbar/UserMenu.tsx
@@ -1,8 +1,8 @@
 "use client"
 import { useCallback, useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
-import Avatar from '../Avatar';
-import MenuItem from './MenuItem';
+import Avatar from '@/components/Avatar';
+import MenuItem from '@/components/navbar/MenuItem';
 const UserMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = useCallback(() => { setIsOpen(!isOpen); }, [isOpen]);
