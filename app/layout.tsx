ed46e6520dbd16d6237646d2d515537590bfa777
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-13 16:07:35 +0100

    Started working on auth modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/navbar/Navbar'
 import ClientOnly from '@/components/ClientOnly'
+import Modal from '@/components/modals/Modal'
 
 const inter = Nunito({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ClientOnly>
+          <Modal isOpen />
           <Navbar />
         </ClientOnly>
         {children}
