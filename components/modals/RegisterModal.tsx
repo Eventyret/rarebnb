5d59ea8ab9220191afab8641fc4e69e0f9bdb057
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-13 16:57:13 +0100

    Working on register form

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -1,8 +1,45 @@
-const RegisterModal = () => {
-  return (
-    <div>
+"use client"
+import { AiFillGithub } from 'react-icons/ai';
+import { FcGoogle } from 'react-icons/fc';
+import useRegisterModal from '@/hooks/useRegisterModal';
+import { useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import Modal from './Modal';
 
-    </div>);
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false)
+  const { register, handleSubmit, formState: { errors } } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    }
+  })
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    setIsLoading(true)
+    try {
+      const response = fetch('/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+      }).then(() => registerModal.onClose())
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+    setIsLoading(false)
+  }
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title='Register' actionLabel='Continue' onClose={registerModal.onClose} onSubmit={handleSubmit(onSubmit)} />
+  );
 }
 
 export default RegisterModal;
--- hooks/useRegisterModal.ts
+++ hooks/useRegisterModal.ts
@@ -11,3 +11,4 @@ const useRegisterModal = create<RegisterModalStore>((set) => ({
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false }),
 }))
+export default useRegisterModal;
