5fed0e462642ab705cc44c7010c55c2ca97428ae
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-13 15:44:33 +0100

    Added fix for hydration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/navbar/Navbar'
+import ClientOnly from '@/components/ClientOnly'
 
 const inter = Nunito({ subsets: ['latin'] })
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
