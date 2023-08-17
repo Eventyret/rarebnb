ef6ba696da65834a237a174780b07258747e0f4f
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-17 13:47:05 +0100

    Fixed logo go to main page

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,9 +1,11 @@
 "use client"
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 const Logo = () => {
+  const router = useRouter();
   return (
-    <Image alt="logo" className='hidden md:block cursor-pointer' height={100} width={100} src={"/rare_logo.png"} />
+    <Image alt="logo" className='hidden md:block cursor-pointer' height={100} width={100} src={"/rare_logo.png"} onClick={() => router.push('/')} />
   );
 }
 
