3e5a8234e3e8c1f8447c1d610a44e2487c4f41e9
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-17 13:39:03 +0100

    Passing Categories and a category box

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import Logo from '@/components/navbar/Logo';
 import Search from '@/components/navbar/Search';
 import UserMenu from '@/components/navbar/UserMenu';
 import { SafeUser } from '@/types';
+import Categories from './Categories';
 
 interface NavbarProps {
   currentUser?: SafeUser | null | undefined;
@@ -11,15 +12,16 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
     <div className='fixed w-full bg-white z-10 shadow-sm'>
-      <div className='p-4 border-b-[1px]'></div>
-      <Container>
-        <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
-          <Logo />
-          <Search />
-          <UserMenu currentUser={currentUser} />
-        </div>
-      </Container>
-
+      <div className='p-4 border-b-[1px]'>
+        <Container>
+          <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
+            <Logo />
+            <Search />
+            <UserMenu currentUser={currentUser} />
+          </div>
+        </Container>
+      </div>
+      <Categories />
     </div>
   );
 }
