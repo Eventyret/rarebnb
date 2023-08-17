295bcc1736157bce7020419d0d2a4fd70dff874e
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-17 14:11:14 +0100

    Started working on steps for rent

--- components/modals/RentModal.tsx
+++ components/modals/RentModal.tsx
@@ -1,11 +1,39 @@
 "use client"
+import { useMemo, useState } from 'react';
+
 import useRentModal from '@/hooks/useRentModal';
 import Modal from './Modal';
 
 const RentModal = () => {
+  enum STEP {
+    CATEGORY = 0,
+    LOCATION = 1,
+    INFO = 2,
+    IMAGES = 3,
+    DESCRIPTION = 4,
+    PRICE = 5
+  }
   const rentModal = useRentModal();
+  const [step, setStep] = useState<STEP>(STEP.CATEGORY);
+
+  const actionLabel = useMemo(() => {
+    if (step === STEP.PRICE) return 'Create';
+    return "Next"
+  }, [step, STEP.PRICE])
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEP.CATEGORY) return undefined
+    return "Back"
+  }, [STEP.CATEGORY, step])
+  const onBack = () => {
+    setStep(value => value - 1)
+  }
+  const onNext = () => {
+    setStep(value => value + 1)
+  }
+
   return (
-    <Modal title='Rarebnb your home!' isOpen={rentModal.isOpen} onClose={rentModal.onClose} onSubmit={rentModal.onClose} actionLabel='Submit' />
+    <Modal title='Rarebnb your home!' isOpen={rentModal.isOpen} onClose={rentModal.onClose} onSubmit={rentModal.onClose} actionLabel={actionLabel} secondaryActionLabel={secondaryActionLabel} secondaryAction={step === STEP.CATEGORY ? undefined : onBack} />
   );
 }
 
