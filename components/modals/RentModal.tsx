83ed41c413a6b733489a71e42d7d9499d52d99a0
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-17 14:41:04 +0100

    Category Input select now working

--- components/modals/RentModal.tsx
+++ components/modals/RentModal.tsx
@@ -6,6 +6,7 @@ import { CATEGORIES } from '@/lib/categories';
 import Heading from '../Heading';
 import Modal from './Modal';
 import CategoryInput from '../inputs/CategoryInput';
+import { FieldValues, useForm } from 'react-hook-form';
 
 const RentModal = () => {
   enum STEP {
@@ -18,6 +19,30 @@ const RentModal = () => {
   }
   const rentModal = useRentModal();
   const [step, setStep] = useState<STEP>(STEP.CATEGORY);
+
+  const { register, handleSubmit, setValue, watch, formState: { errors }
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: '',
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: '',
+      price: 1,
+      title: '',
+      description: ''
+    }
+  });
+  const category = watch('category');
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true
+    })
+  }
 
   const actionLabel = useMemo(() => {
     if (step === STEP.PRICE) return 'Create';
@@ -35,16 +60,34 @@ const RentModal = () => {
     setStep(value => value + 1)
   }
 
+
   let bodyContent = (
-    <div className='flex flex-col gap-8'>
-      <Heading title='Which of these best describes your place?' subtitle='Pick a category' />
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto'>
-        {CATEGORIES.map((category) => (
-          <div key={category.label} className='col-span-1'>
-            <CategoryInput onClick={() => { }} selected={false} label={category.label} icon={category.icon} />
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Which of these best describes your place?"
+        subtitle="Pick a category"
+      />
+      <div
+        className="
+          grid 
+          grid-cols-1 
+          md:grid-cols-2 
+          gap-3
+          max-h-[50vh]
+          overflow-y-auto
+        "
+      >
+        {CATEGORIES.map((item) => (
+          <div key={item.label} className="col-span-1">
+            <CategoryInput
+              onClick={(category) =>
+                setCustomValue('category', category)}
+              selected={category === item.label}
+              label={item.label}
+              icon={item.icon}
+            />
           </div>
-        ))
-        }
+        ))}
       </div>
     </div>
   )
