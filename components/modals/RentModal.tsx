5d91dc7cf8d1c0273b8130af7702794f5f8861ea
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-17 14:21:38 +0100

    Created modal for rents

--- components/modals/RentModal.tsx
+++ components/modals/RentModal.tsx
@@ -2,6 +2,8 @@
 import { useMemo, useState } from 'react';
 
 import useRentModal from '@/hooks/useRentModal';
+import { CATEGORIES } from '@/lib/categories';
+import Heading from '../Heading';
 import Modal from './Modal';
 
 const RentModal = () => {
@@ -32,8 +34,29 @@ const RentModal = () => {
     setStep(value => value + 1)
   }
 
+  let bodyContent = (
+    <div className='flex flex-col gap-8'>
+      <Heading title='Which of these best describes your place?' subtitle='Pick a category' />
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto'>
+        {CATEGORIES.map((category) => (
+          <div key={category.label} className='col-span-1'>
+            {category.label}
+          </div>
+        ))
+        }
+      </div>
+    </div>
+  )
+
   return (
-    <Modal title='Rarebnb your home!' isOpen={rentModal.isOpen} onClose={rentModal.onClose} onSubmit={rentModal.onClose} actionLabel={actionLabel} secondaryActionLabel={secondaryActionLabel} secondaryAction={step === STEP.CATEGORY ? undefined : onBack} />
+    <Modal
+      title='Rarebnb your home!'
+      isOpen={rentModal.isOpen}
+      onClose={rentModal.onClose}
+      onSubmit={rentModal.onClose}
+      actionLabel={actionLabel}
+      secondaryActionLabel={secondaryActionLabel} secondaryAction={step === STEP.CATEGORY ? undefined : onBack}
+      body={bodyContent} />
   );
 }
 
