a404863f9d11bd39cdec8a82df240c54e23a0918
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-17 13:45:10 +0100

    Added all categories

--- components/navbar/Categories.tsx
+++ components/navbar/Categories.tsx
@@ -1,29 +1,10 @@
 "use client"
 
-import Container from '../Container'
-import { TbBeach } from 'react-icons/tb'
-import { GiWindmill } from 'react-icons/gi'
-import { MdOutlineVilla } from 'react-icons/md'
-import CategoryBox from './CategoryBox'
+import { CATEGORIES } from '@/lib/categories'
 import { usePathname, useSearchParams } from 'next/navigation'
+import Container from '../Container'
+import CategoryBox from './CategoryBox'
 
-export const categories = [
-  {
-    label: 'Beach',
-    icon: TbBeach,
-    description: "This property is close to the beach!"
-  },
-  {
-    label: 'Windmills',
-    icon: GiWindmill,
-    description: "This property has windmills!"
-  },
-  {
-    label: 'Modern',
-    icon: MdOutlineVilla,
-    description: "This property is modern!"
-  }
-]
 
 const Categories = () => {
   const params = useSearchParams()
@@ -38,7 +19,7 @@ const Categories = () => {
   return (
     <Container>
       <div className='pt-4 flex flex-row items-center justify-between overflow-x-auto'>
-        {categories.map((item, index) => (
+        {CATEGORIES.map((item, index) => (
           <CategoryBox key={index} label={item.label} selected={category === item.label} icon={item.icon} />
         ))}
       </div>
