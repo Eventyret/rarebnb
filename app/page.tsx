16bef85c75a5131ded84cbe664c9aefb25633366
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-19 17:21:32 +0100

    Working on ListingCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import getCurrentUser from '@/actions/getCurrentUser'
 import getListings from '@/actions/getListings'
 import ClientOnly from '@/components/ClientOnly'
 import Container from '@/components/Container'
 import EmptyState from '@/components/EmptyState'
+import ListingCard from '@/components/listings/ListingCard'
+import { Listing } from '@prisma/client'
 
 export default async function Home() {
   const listings = await getListings()
+  const currentUser = await getCurrentUser()
   if (listings.length === 0) {
     return (
       <ClientOnly>
@@ -16,7 +20,10 @@ export default async function Home() {
     <ClientOnly>
       <Container>
         <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-          <div>My Future listings </div>
+          {listings?.map((listing: Listing) => (
+            <ListingCard key={listing.id} listing={listing} currentUser={currentUser} />
+
+          ))}
         </div>
 
       </Container>
