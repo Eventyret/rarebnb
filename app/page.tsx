b840e950c0c2e45d0450fc2be65ca0505104fec0
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-19 16:46:29 +0100

    Started working on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import ClientOnly from '@/components/ClientOnly'
+import Container from '@/components/Container'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>Main Content</main>
+    <ClientOnly>
+      <Container>
+        <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+          <div>My Future listings </div>
+        </div>
+
+      </Container>
+    </ClientOnly>
   )
 }
